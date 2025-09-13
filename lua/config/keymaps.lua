f92dfc00b4bb807277b1dff7255a54288dc2566e
ksuomala <ksuomala@student.hive.fi>
2025-09-13 23:46:52 +0200

    move keybindings to one file

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,7 @@
 --make space the leader key
 vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
+vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
+vim.keymap.set("n", "<space>fd", "<cmd>Telescope find_files<CR>", { desc = "Find files" })
+vim.keymap.set("n", "<space>fg", "<cmd>Telescope live_grep<CR>", { desc = "Live grep" })
+
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -5,11 +5,6 @@ return {
     "nvim-lua/plenary.nvim",
     { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
   },
-  -- better: put keymaps here so lazy can load Telescope on demand
-  keys = {
-    { "<space>fd", function() require("telescope.builtin").find_files() end, desc = "Find files" },
-    { "<space>fg", function() require("telescope.builtin").live_grep() end,  desc = "Live grep"  },
-  },
   config = function()
     require("telescope").setup({})
     pcall(require("telescope").load_extension, "fzf")
